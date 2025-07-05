59eadf6f5b9d4ca0957c439719ec8a8a7f6cf0bb
crj007 <chiranjivsinha0295@gmail.com>
2025-07-05 11:17:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -173,4 +173,11 @@ function showSadFace() {
   document.body.appendChild(sad);
   setTimeout(() => sad.remove(), 2000);
 }
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
